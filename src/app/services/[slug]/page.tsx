e46f8a26c530545c260afe0d43433af545117fc6
Janos Csikos <janos@csikos.co.uk>
2025-09-03 14:48:21 +0200

    Fixing deployment error

--- src/app/services/[slug]/page.tsx
+++ src/app/services/[slug]/page.tsx
@@ -79,24 +79,6 @@ export default async function ServiceDetailPage({ params }: ServicePageProps) {
     'data-and-analytics': '/img/analyse.jpg',
   };
 
-  const iconMapping: { [key: string]: React.ReactNode } = {
-    'customer-experience': (
-      <svg className="w-16 h-16 text-white" fill="currentColor" viewBox="0 0 24 24">
-        <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-2 15l-5-5 1.41-1.41L10 14.17l7.59-7.59L19 8l-9 9z"/>
-      </svg>
-    ),
-    'operational-efficiency': (
-      <svg className="w-16 h-16 text-white" fill="currentColor" viewBox="0 0 24 24">
-        <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"/>
-      </svg>
-    ),
-    'data-and-analytics': (
-      <svg className="w-16 h-16 text-white" fill="currentColor" viewBox="0 0 24 24">
-        <path d="M3 3v18h18v-2H5V3H3z"/>
-        <path d="M7 12l4-4 4 4 4-4v3l-4 4-4-4-4 4V12z"/>
-      </svg>
-    ),
-  };
 
   return (
     <Layout>
